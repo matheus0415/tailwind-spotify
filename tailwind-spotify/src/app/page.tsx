eedc52eea60e256aca642a3a05174141376cf177
Matheus Santos <matheus.santos@fitbank.com.br>
2024-03-27 17:51:09 -0300

    style: estrutura inicial
    #main

--- tailwind-spotify/src/app/page.tsx
+++ tailwind-spotify/src/app/page.tsx
@@ -1,3 +1,39 @@
+import { Home as HomeIcon, Search, Library } from "lucide-react";
+
 export default function Home() {
-  return <h1>Hello world!</h1>;
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-72 bg-zinc-950 p-6">
+          <nav className="space-y-4">
+            <a
+              href=""
+              className="flex items-center gap-2 font-semibold text-zinc-200"
+            >
+              <HomeIcon />
+              Home
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-2 font-semibold text-zinc-200"
+            >
+              <Search />
+              Search
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-2 font-semibold text-zinc-200"
+            >
+              <Library />
+              Your Library
+            </a>
+          </nav>
+        </aside>
+        <main className="flex-1 p-6">main </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        footer
+      </footer>
+    </div>
+  );
 }
