5020a86d5511bb07a855bd1b503d36f96cf6341f
Matheus Santos <matheus.santos@fitbank.com.br>
2024-03-28 16:12:53 -0300

    style: finalizando sidebar

--- tailwind-spotify/src/app/page.tsx
+++ tailwind-spotify/src/app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
-          <nav className="space-y-4">
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+            <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+          </div>
+          <nav className="space-y-5 mt-10">
             <a
               href=""
               className="flex items-center gap-2 font-semibold text-zinc-200"
@@ -26,6 +31,24 @@ export default function Home() {
             >
               <Library />
               Your Library
+            </a>
+          </nav>
+
+          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Hot hits
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              This is Arctic Monkeys
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Your Mix
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Forró das Antigas
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              HipHop remix
             </a>
           </nav>
         </aside>
